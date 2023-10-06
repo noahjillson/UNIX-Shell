7f74a4f5f1a52435702f0cdca10870ffeeb4bed0
Noah Jillson <noahjillson@gmail.com>
2023-10-06 15:36:27 -0500

    Duplicates input file to stdin fd for batch mode

--- wsh.c
+++ wsh.c
@@ -1,31 +1,24 @@
+#include <stdlib.h>
 #include <stdio.h>
-#include <wsh.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "./wsh.h"
 
 int execute(char* command) {
-    printf("Hello world\n");
+    printf("%s\n", command);
+    return 0;
 }
 
-// int batch(char* fileName) {
-
-// }
-
-// int interactive() {
-//     int fd = 0;
-//     int cap = 256;
-//     char line[cap];
-//     while (-1 != (line = getline(&line, &cap, )))
-// }
-
-
-
 int listen(SHELL_MODE mode) {
-    int cap = 256;
-    char line[cap];
-    while (-1 != (line = getline(&(line[0]), &cap, stdin))) {
+    int len = 0;
+    size_t cap = 256;
+    char* line = NULL;
+    // getline mallocs a new buffer for us and will realloc() as necessary to handle longer lines
+    while (-1 != (len = getline(&line, &cap, stdin))) {
         if (INTERACTIVE == mode) {
             printf("wsh> ");
         }
-        execute(&(line[0]));
+        execute(line);
     }
     return 0;
 }
@@ -37,11 +30,17 @@ int main(int argc, char** argv) {
     }
     // batch shell mode
     else if (2 == argc) {
-        FILE *fd = fopen(argv[1], "r");
-        dup2(stdin, fd);
+        int fd = open(argv[1], O_RDONLY);
+        if (fd < 0) {
+            printf("Error opening file %s.\n", argv[1]);
+            exit(1);
+        }
+
+        dup2(fd, 0);
+        close(fd);
         return listen(BATCH);
     }
 
-    printf("Invalid usage of %s with %d arguments", argv[0], argc);
+    printf("Invalid usage of %s with %d arguments.\n", argv[0], argc);
     exit(1);
 }
