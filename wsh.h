c9762362a87a06eb4499b2076009c4efeb15d095
Noah Jillson <noahjillson@gmail.com>
2023-10-08 22:46:50 -0500

    MAX_ARG_CNT macro

--- wsh.h
+++ wsh.h
@@ -1,17 +1,17 @@
+// Maximum argument count supported for a given command
+#define MAX_ARG_CNT 256
+
 // Defining different modes the shell can be run in
 typedef const int SHELL_MODE;
 SHELL_MODE INTERACTIVE = 0;
 SHELL_MODE BATCH = 1;
-
-// Maximum argument count supported for a given command
-const int MAX_ARG_CNT = 256;
 
 // Job structre for keeping track of the processes started from shell
 struct JOB {
     pid_t pid;
     int jid;
     char *pname;
-    char *pargv[256];
+    char *pargv[MAX_ARG_CNT];
     struct JOB *pipeNext;
     struct JOB *next;
     struct JOB *prev;
